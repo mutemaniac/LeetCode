9615e552ed87b59ae3f5ca0b45699d1eeca4d48d
TangQian <qian.tang@hnair.com>
2017-02-15 14:37:43 +0800

    done

--- PacificAtlanticWaterFlow/main.c
+++ PacificAtlanticWaterFlow/main.c
@@ -4,71 +4,194 @@
 
 #define pacific (0x1)
 #define atlantic (0x2)
-#define both (0x3)
+#define both(x) ((x&0x3)==0x3)
 #define land (0x4)
+#define counted (0x8)
+#define left (1)
+#define right (2)
+#define up (3)
+#define down (4)
 
 
+unsigned char fromfrom(int **matrix, int matrixRowSize, int matrixColSize,
+                   unsigned char **map, int x, int y, int from,
+                   int *returnSize)
+{
+    //Have computed.
+    if (!(map[x][y]&0x3))
+    {
+        return 0;
+    }
+    // if(both(map[x][y])){
+    //     *returnSize += 1;
+    // }
+    //printf("compute one (%d, %d)=%d\n", x, y, map[x][y]);
 
-unsigned char gogo(int** matrix, int matrixRowSize, int matrixColSize, 
-                    unsigned char** map, int x, int y, 
-                    int* returnSize){
-    if(map[x][y]){
+    if(x>0)
+    {
+        //x-1
+        if (matrix[x][y] <= matrix[x - 1][y] && (map[x][y]|map[x-1][y])>map[x-1][y] )
+        { // Can flow to x-1
+            map[x-1][y] |= map[x][y];
+            fromfrom(matrix, matrixRowSize, matrixColSize, map, x-1, y, down, returnSize);
+        }
+    }
+    if(x+1 < matrixRowSize)
+    {
+        //x+1
+        if (matrix[x][y] <= matrix[x + 1][y] && (map[x][y]|map[x+1][y])>map[x+1][y] )
+        { // Can flow to x+1
+            map[x+1][y] |= map[x][y];
+            fromfrom(matrix, matrixRowSize, matrixColSize, map, x+1, y, down, returnSize);
+        }
+    }
+    
+    if(y>0)
+    {
+        //y-1
+        if (matrix[x][y] <= matrix[x][y-1] && (map[x][y]|map[x][y-1])>map[x][y-1] )
+        { // Can flow to x+1
+            map[x][y-1] |= map[x][y];
+            fromfrom(matrix, matrixRowSize, matrixColSize, map, x, y-1, down, returnSize);
+        }
+    }
+    if(y+1 < matrixColSize)
+    {
+        //y+1
+        if (matrix[x][y] <= matrix[x][y+1] && (map[x][y]|map[x][y+1])>map[x][y+1] )
+        { // Can flow to x+1
+            map[x][y+1] |= map[x][y];
+            fromfrom(matrix, matrixRowSize, matrixColSize, map, x, y+1, down, returnSize);
+        }
+    }
+
+    return map[x][y];
+}
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int **pacificAtlantic(int **matrix, int matrixRowSize, int matrixColSize, int **columnSizes, int *returnSize)
+{
+    *returnSize = 0;
+    unsigned char **map = (unsigned char **)malloc(sizeof(unsigned char *) * matrixRowSize);
+    for (int i = 0; i < matrixRowSize; i++)
+    {
+        map[i] = (unsigned char *)malloc(sizeof(unsigned char) * matrixColSize);
+        memset(map[i], 0, (size_t)matrixColSize);
+        map[i][0] |= pacific;
+        map[i][matrixColSize-1] |= atlantic;
+    }
+    for (int j = 0; j < matrixColSize; j++)
+    {
+        map[0][j] |= pacific;
+        map[matrixRowSize-1][j] |= atlantic;
+    }
+
+    for (int i = 0; i < matrixRowSize; i++)
+    {
+        fromfrom(matrix, matrixRowSize, matrixColSize, map, i, 0, 0, returnSize);
+        fromfrom(matrix, matrixRowSize, matrixColSize, map, i, matrixColSize-1, 0, returnSize);
+    }
+    for (int j = 0; j < matrixColSize; j++)
+    {
+        fromfrom(matrix, matrixRowSize, matrixColSize, map, 0, j, 0, returnSize);
+        fromfrom(matrix, matrixRowSize, matrixColSize, map, matrixRowSize-1, j, 0, returnSize);
+    }
+
+    int **ret = (int **)malloc(sizeof(int *) * (matrixRowSize*matrixColSize));
+    int *retcs = (int *)malloc(sizeof(int) * (matrixRowSize*matrixColSize));
+    int k = 0;
+    for (int i = 0; i < matrixRowSize; i++)
+    {
+        for (int j = 0; j < matrixColSize; j++)
+        {
+            if (both(map[i][j]))
+            {
+                //
+                ret[k] = (int *)malloc(sizeof(int) * 2);
+                ret[k][0] = i;
+                ret[k][1] = j;
+                retcs[k] = 2;
+                k++;
+            }
+        }
+    }
+    *returnSize = k;
+    *columnSizes = retcs;
+    return ret;
+}
+
+unsigned char gogo(int **matrix, int matrixRowSize, int matrixColSize,
+                   unsigned char **map, int x, int y, int from,
+                   int *returnSize)
+{
+    //Have computed.
+    if (map[x][y])
+    {
         return map[x][y];
     }
-    if(x==0){
+    printf("compute one (%d, %d)\n", x, y);
+    if (x == 0)
+    {
         map[x][y] = map[x][y] | pacific;
-    }else{
+    }
+    else
+    {
         //x-1
-        if(matrix[x][y] >= matrix[x-1][y]){// Can flow to x-1
-            map[x][y] |= gogo(matrix, matrixRowSize, matrixColSize, map, x-1, y, returnSize);
-            // if((map[x][y]&both) == both ){
-            //     //Can flow to both the Pacific and Atlantic ocean 
-            //     return map[x][y];
-            // }
+        if (matrix[x][y] >= matrix[x - 1][y] && from!=up)
+        { // Can flow to x-1
+            map[x][y] |= gogo(matrix, matrixRowSize, matrixColSize, map, x - 1, y, down, returnSize);
         }
     }
-    if(x == (matrixRowSize-1)){
+    if (x == (matrixRowSize - 1))
+    {
         map[x][y] = map[x][y] | atlantic;
-    }else{
+    }
+    else
+    {
         //x+1
-        if(matrix[x][y] >= matrix[x+1][y]){ // Can flow to x+1
-            map[x][y] |= gogo(matrix, matrixRowSize, matrixColSize, map, x+1, y, returnSize);
-            // if((map[x][y]&both) == both ){
-            //     //Can flow to both the Pacific and Atlantic ocean 
-            //     return map[x][y];
-            // }
+        if (matrix[x][y] >= matrix[x + 1][y] && from!=down)
+        { // Can flow to x+1
+            map[x][y] |= gogo(matrix, matrixRowSize, matrixColSize, map, x + 1, y, up, returnSize);
         }
     }
-    if(y == 0){
+    if (y == 0)
+    {
         map[x][y] = map[x][y] | pacific;
-    }else{
+    }
+    else
+    {
         //y-1
-        if(matrix[x][y] >= matrix[x][y-1]){ // Can flow to y-1
-            map[x][y] |= gogo(matrix, matrixRowSize, matrixColSize, map, x, y-1, returnSize);
-            // if((map[x][y]&both) == both ){
-            //     //Can flow to both the Pacific and Atlantic ocean 
-            //     return map[x][y];
-            // }
+        if (matrix[x][y] >= matrix[x][y - 1] && from != right)
+        { // Can flow to y-1
+            map[x][y] |= gogo(matrix, matrixRowSize, matrixColSize, map, x, y - 1, left, returnSize);
         }
     }
-    if(y == (matrixColSize-1)){
+    if (y == (matrixColSize - 1))
+    {
         map[x][y] = map[x][y] | atlantic;
-    }else{
+    }
+    else
+    {
         //y+1
-        if(matrix[x][y] >= matrix[x][y-1]){ // Can flow to y+1
-            map[x][y] |= gogo(matrix, matrixRowSize, matrixColSize, map, x, y+1, returnSize);
-            // if((map[x][y]&both) == both ){
-            //     //Can flow to both the Pacific and Atlantic ocean 
-            //     return map[x][y];
-            // }
+        if (matrix[x][y] >= matrix[x][y + 1]  && from != left )
+        { // Can flow to y+1
+            map[x][y] |= gogo(matrix, matrixRowSize, matrixColSize, map, x, y + 1,right, returnSize);
         }
     }
 
-    if(map[x][y]==0){
+    if (map[x][y] == 0)
+    {
         map[x][y] = land;
-    }else  if((map[x][y]&both) == both ){
-        //Can flow to both the Pacific and Atlantic ocean 
+    }
+    else if ( both(map[x][y]) && ((map[x][y] & counted) != counted))
+    {
+        //Can flow to both the Pacific and Atlantic ocean
         *returnSize += 1;
+        map[x][y] |= counted;
     }
     return map[x][y];
 }
@@ -78,27 +201,34 @@ unsigned char gogo(int** matrix, int matrixRowSize, int matrixColSize,
  * The sizes of the arrays are returned as *columnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int** pacificAtlantic(int** matrix, int matrixRowSize, int matrixColSize, int** columnSizes, int* returnSize) {
+int **pacificAtlantic1(int **matrix, int matrixRowSize, int matrixColSize, int **columnSizes, int *returnSize)
+{
     *returnSize = 0;
-    unsigned char** map = (unsigned char**)malloc(sizeof(unsigned char*) * matrixRowSize);
-    for(int i=0;i<matrixRowSize;i++){
-        *map = (unsigned char*)malloc(sizeof(unsigned char)*matrixColSize);
-        memset(map, 0, (size_t)matrixColSize);
+    unsigned char **map = (unsigned char **)malloc(sizeof(unsigned char *) * matrixRowSize);
+    for (int i = 0; i < matrixRowSize; i++)
+    {
+        map[i] = (unsigned char *)malloc(sizeof(unsigned char) * matrixColSize);
+        memset(map[i], 0, (size_t)matrixColSize);
     }
-    for(int i=0; i<matrixRowSize; i++){
-        for(int j=0; j<matrixColSize; i++){
-            gogo(matrix, matrixRowSize, matrixColSize, map, i, j, returnSize);
+    for (int i = 0; i < matrixRowSize; i++)
+    {
+        for (int j = 0; j < matrixColSize; j++)
+        {
+            gogo(matrix, matrixRowSize, matrixColSize, map, i, j, 0, returnSize);
         }
     }
 
-    int** ret = (int**)malloc(sizeof(int*) * (*returnSize));
-    int* retcs = (int*)malloc(sizeof(int) * (*returnSize));
+    int **ret = (int **)malloc(sizeof(int *) * (*returnSize));
+    int *retcs = (int *)malloc(sizeof(int) * (*returnSize));
     int k = 0;
-    for(int i=0; i<matrixRowSize; i++){
-        for(int j=0; j<matrixColSize; i++){
-            if((map[i][j]&both) == both){
+    for (int i = 0; i < matrixRowSize; i++)
+    {
+        for (int j = 0; j < matrixColSize; j++)
+        {
+            if (both(map[i][j]))
+            {
                 //
-                ret[k] = (int*)malloc(sizeof(int)*2);
+                ret[k] = (int *)malloc(sizeof(int) * 2);
                 ret[k][0] = i;
                 ret[k][1] = j;
                 retcs[k] = 2;
@@ -111,8 +241,24 @@ int** pacificAtlantic(int** matrix, int matrixRowSize, int matrixColSize, int** 
     return ret;
 }
 
-int main(){
-
+int main()
+{
+    const int x = 5;
+    const int y = 5;
+    int data[x][y] = {{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
+    //int data[x][y] = {{1, 1}, {1, 1}, {1, 1}};
+    int** matrix = (int**)malloc(sizeof(int*)*x);
+    for(int i=0;i<x;i++){
+        matrix[i] = data[i];
+    }
+    
+    int *columnSizes;
+    int returnSize;
+    int** ret = pacificAtlantic(matrix, x, y, &columnSizes, &returnSize);
+    for (int i = 0; i < returnSize; i++)
+    {
+        printf("x=%d, y=%d\n", ret[i][0], ret[i][1]);
+    }
 
     return 0;
 }
